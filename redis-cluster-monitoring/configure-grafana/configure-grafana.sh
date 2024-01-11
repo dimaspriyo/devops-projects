9f98eb86b1725a0acfb634cfd97fca4b3d3191b4
dimas <dimas.priyo@bluebirdgroup.com>
2024-01-11 10:32:27 +0700

    Fix import redis datasource

--- redis-cluster-monitoring/configure-grafana/configure-grafana.sh
+++ redis-cluster-monitoring/configure-grafana/configure-grafana.sh
@@ -2,12 +2,7 @@
 
 sleep 30
 
-curl -v -X POST -H 'Content-Type: application/json' -d ' { "name":"api-key", "role":"Admin", "secondsToLive":86400 }' http://admin:admin@grafana:3000/api/auth/keys && curl -v -X POST  -H "Content-Type: application/json" -d @datasources/redis-source.json http://127.0.0.1:3000/api/datasources
-# wget --no-check-certificate --quiet --timeout=0 --header='Content-Type: application/json' --post-data='{ "name":"api-key", "role":"Admin", "secondsToLive":86400}' 'http://admin:admin@grafana:3000/api/auth/keys'
-
-# curl -v -X POST -H 'Content-Type: application/json' -d ' { "id":null, "orgId":1, "folderId":null, "overwrite":false, "dashboard":{ "id":null, "uid":null, "title":"Sample Dashboard", "timezone":"browser", "schemaVersion":27, "version":0, "time":{"from":"now-6h","to":"now"}, "refresh":"10s", "panels":[], "annotations":{ "list":[] }, "hideControls":false }, "folderId":0, "overwrite":false }' http://admin:admin@grafana:3000/api/dashboards/db
-# wget --no-check-certificate --quiet --timeout=0 --header='Content-Type: application/json' --post-data=' { "id":null, "orgId":1, "folderId":null, "overwrite":false, "dashboard":{ "id":null, "uid":null, "title":"Sample Dashboard", "timezone":"browser", "schemaVersion":27, "version":0, "time":{"from":"now-6h","to":"now"}, "refresh":"10s", "panels":[], "annotations":{ "list":[] }, "hideControls":false }, "folderId":0, "overwrite":false }' 'http://admin:admin@grafana:3000/api/dashboards/db'
-
+curl -v -X POST -H 'Content-Type: application/json' -d ' { "name":"api-key", "role":"Admin", "secondsToLive":86400 }' http://admin:admin@grafana:3000/api/auth/keys && curl -v -X POST  -H "Content-Type: application/json" -d @datasources/redis-source.json http://grafana:3000/api/datasources
 
 # Function to create Grafana API key
 create_api_key() {
@@ -15,7 +10,7 @@ create_api_key() {
 
   echo "Creating API key: $api_key_name"
   curl -X POST -H "Authorization: Bearer admin_password" -H "Content-Type: application/json" \
-    -d "{\"name\": \"$api_key_name\", \"role\": \"$api_key_role\"}" http://127.0.0.1:3000/auth/keys
+    -d "{\"name\": \"$api_key_name\", \"role\": \"$api_key_role\"}" http://grafana:3000/auth/keys
 }
 
 # Create predefined service accounts
