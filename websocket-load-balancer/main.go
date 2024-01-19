a83993a19c2b4bd0b63fe4f12a626a43da96e07e
dimas <dimas.priyo@bluebirdgroup.com>
2024-01-19 11:04:57 +0700

    Add redis store

--- websocket-load-balancer/main.go
+++ websocket-load-balancer/main.go
@@ -2,18 +2,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
 )
+
+type Handler struct {
+	mux         http.ServeMux
+	redisClient redis.Client
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func handleConnection(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) handleConnection(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -22,28 +31,47 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	for {
-		messageType, p, err := conn.ReadMessage()
+		messageType, _, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
+		counter, err := h.redisClient.Incr(context.Background(), "counter").Result()
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println("From Redis: ", counter)
+
+		resp := strconv.Itoa(int(counter))
+		if err := conn.WriteMessage(messageType, []byte(resp)); err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(string(p))
 	}
 }
 
-func healthCheck(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	http.HandleFunc("/", handleConnection)
-	http.HandleFunc("/health-check", healthCheck)
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	handler := Handler{
+		mux:         *http.DefaultServeMux,
+		redisClient: *redisClient,
+	}
+
+	handler.mux.HandleFunc("/", handler.handleConnection)
+	handler.mux.HandleFunc("/health-check", handler.healthCheck)
 	port := "9100"
 	fmt.Println("Server is running on :", port)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), &handler.mux)
 }
