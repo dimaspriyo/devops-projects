06c941ab77c7158e5bd335a1c50a00544b0f3e3e
dimas <dimas.priyo@bluebirdgroup.com>
2024-01-30 13:53:41 +0700

    Add event handler to add data in div in onmessage

--- websocket-load-balancer/frontend/src/App.vue
+++ websocket-load-balancer/frontend/src/App.vue
@@ -1,36 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
 
-const socket = new WebSocket('ws://localhost:9200')
-
-const handleConnect = () => {
-  if(username.value.length > 0) {
-    connect.value = true
-  }
+const items = ref([])
+const socket = new WebSocket("ws://localhost:9100")
+const sendMessage = (event) => {
+  socket.send("ok")
+  console.log("send to websocket")
 }
 
-const sendMessage = (event) => {
-  console.log("send to websocket")
-        socket.send("ok")
-      }
+socket.onmessage = (event) => {
+  console.log("onmessage: ", event.data)
+  addItem(event.data)
+}
 
-      socket.onmessage = (event) => {
-        console.log("onmessage: ", event.data)
-        
-      }
 
-  //     socket.onclose = function(e) {
-  //   console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-  //   setTimeout(function() {
-  //      socket = new WebSocket('ws://localhost:9100')
-  //   }, 100);
-  // };
+const list = ref([]);
 
-  // socket.onerror = function(err) {
-  //   console.error('Socket encountered error: ', err.message, 'Closing socket');
-  //   socket.close();
-  // };
+
+const addItem = (data) => {
+  list.value.push({message:data});
+};
+
 
 </script>
 
@@ -38,21 +28,22 @@ const sendMessage = (event) => {
   <div class="chat-container">
     <!-- Name Input window -->
     <div>
-        <div class="modal-background">
-          <div class="modal-content">
-            <form @submit.prevent="handleSubmit" style="text-align: center;">
-              <h3> Counter </h3>
-              <h3> 1 </h3>
-              <!-- <input type="text" v-model="username" placeholder="Enter your name" /> -->
-              <br>
-              <button type="submit" @click="sendMessage"> Connect </button>
-            </form>
-          </div>
+      <div class="modal-background">
+        <div class="modal-content">
+          <form @submit.prevent="handleSubmit" style="text-align: center;">
+            <h3> Counter </h3>
+            <h3> 1 </h3>
+            <br>
+            <button type="submit" @click="sendMessage"> Connect</button>
+          </form>
         </div>
       </div>
-      <br/>
-    <div style="width: 400px; height: 600px; border: 1px solid black; text-align: center;">
-      History Goes Down Here !
+    </div>
+    <br/>
+    <div  style="width: 400px; height: 600px; border: 1px solid black; text-align: center;">
+      <p v-for="item in list">
+        {{item.message}}
+      </p>
     </div>
   </div>
 
